67ddee4858013d6b5939f984e6168436087be703
Kovalchishin Kirill <kovalchishin.kb@gmail.com>
2023-07-04 14:12:45 +0300

    Initial commit

---
+++ src/test/java/com/kovalchishin/SpringWebApp/SpringWebAppTests.java
@@ -0,0 +1,13 @@
+package com.kovalchishin.SpringWebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
